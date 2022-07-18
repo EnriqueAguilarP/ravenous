bb39f7828d248898f4622554371dbf062b1a849e
EnriqueAguilarP <dialectikos@gmail.com>
2022-07-18 15:48:59 -0500

    Add search function

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,18 @@
 const apiKey ='fCQRoiM3-VMHKw1LbFWEcobZ4xolYfC8HPLn95_IjAM21eOtLZgpYOXAtTyhDmSGI6Gc1Uvv6x7_wV4dnrxwcQTSKdxDEgXz95PvvatLBtc8OIvd-dV8I45jD8DVYnYx';
-const Yelp = {};
+const Yelp = {
+    searchYelp(term, location, sortBy){
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+            {headers: {
+                Authorization: `Bearer ${apiKey}`
+            }}).then((response)=>{
+                return response.json();
+            }).then((jsonResponse)=>{
+                if(jsonResponse.businesses){
+                    return jsonResponse.businesses.map((business)=>{
+
+                    })
+                }else{}
+            })
+
+    }
+};
